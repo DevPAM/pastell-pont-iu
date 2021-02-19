0ac28068fad3fa950bc3ffc7756ea0abf6f05da5
Baobab <44770087+DevPAM@users.noreply.github.com>
2021-02-19 09:19:25 +0100

    Commit nodes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MatInputModule} from '@angular/material/input';
+
+import {FormsModule, ReactiveFormsModule/*, FormGroupDirective, NgForm, Validators*/ } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { AuthenticationViewComponent } from './components/authentication/authentication-view/authentication-view.component';
+import { InscriptionViewComponent } from './components/authentication/inscription-view/inscription-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    AuthenticationViewComponent,
+    InscriptionViewComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/authentication/inscription-view/inscription-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inscription-view',
+  templateUrl: './inscription-view.component.html',
+  styleUrls: ['./inscription-view.component.css']
+})
+export class InscriptionViewComponent {
+  constructor() { }
+}
